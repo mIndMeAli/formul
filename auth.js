04ca27b6fd5864b5c18083da308f8defa910ed30
mIndMeAli <160200397+mIndMeAli@users.noreply.github.com>
2025-03-21 09:30:49 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,16 @@
-// Cek apakah user sudah login
-if (!localStorage.getItem("user")) {
+// Cek apakah user sudah login berdasarkan localStorage
+if (!localStorage.getItem("loggedIn")) {
     window.location.href = "index.html"; // Jika belum login, kembali ke login page
+} else {
+    // Tampilkan nama PIC di halaman form
+    document.getElementById("picName").textContent = localStorage.getItem("pic") || "Tidak Diketahui";
 }
 
 // Logout function
 document.getElementById("logoutBtn").addEventListener("click", function() {
-    localStorage.removeItem("user"); // Hapus session
+    localStorage.removeItem("loggedIn"); 
+    localStorage.removeItem("jenisPengusulan");
+    localStorage.removeItem("pic");
+    
     window.location.href = "index.html"; // Redirect ke login
 });
